1ec4579d41e3cea472f7216dd3b72a51c7aa769a
Gabriel Turrillas <gabrielturrillasdev@gmail.com>
2021-10-28 14:54:38 -0300

    boilerplate

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+import { viewportsSizes } from "../styles/viewports";
+import '../styles/globals.css'
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  layout: 'fullscreen',
+  viewport: {
+    viewports: viewportsSizes
+  },
+};
